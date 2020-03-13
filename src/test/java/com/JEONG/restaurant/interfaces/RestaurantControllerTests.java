a04b4402108a6c520d0d9a21492c788aea649ae6
seohwa jeong <suhaw0325@naver.com>
2020-03-13 15:56:43 +0900

    a

---
+++ src/test/java/com/JEONG/restaurant/interfaces/RestaurantControllerTests.java
@@ -0,0 +1,44 @@
+package com.JEONG.restaurant.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RestaurantController.class)
+public class RestaurantControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bob zip\"")
+                ));
+
+    }
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":2020")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Cyber Food\"")
+                ));
+    }
+
+
+}
