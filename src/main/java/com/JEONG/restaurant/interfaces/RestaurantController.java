2da7ef827e268d29e34d9ac6f9540a1fb6f3e397
seohwa jeong <suhaw0325@naver.com>
2020-03-16 18:33:59 +0900

    two

--- src/main/java/com/JEONG/restaurant/interfaces/RestaurantController.java
+++ src/main/java/com/JEONG/restaurant/interfaces/RestaurantController.java
@@ -3,10 +3,14 @@ package com.JEONG.restaurant.interfaces;
 import com.JEONG.restaurant.application.RestaurantService;
 import com.JEONG.restaurant.domain.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -25,12 +29,13 @@ public class RestaurantController {
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id")Long id){
         Restaurant restaurant =restaurantService.getRestaurant(id);
-//        Restaurant restaurant= restaurantRepository.findById(id);
-//
-//        List<MenuItem> menuItems =menuItemRepository.findAllByRestaurantId(id);
-//        restaurant.setMenuItems(menuItems);
-
-
         return restaurant ;
+    }
+    @PostMapping("/restaurants")
+    public ResponseEntity<?> create() throws URISyntaxException {
+        Restaurant restaurant = new Restaurant(1234L,"BeRyong","Busan");
+        restaurantService.addRestaurant(restaurant);
+        URI location = new URI("/restaurants/1234");
+        return ResponseEntity.created(location).body("{}");
     }
 }
--- src/test/java/com/JEONG/restaurant/interfaces/RestaurantControllerTests.java
+++ src/test/java/com/JEONG/restaurant/interfaces/RestaurantControllerTests.java
@@ -13,10 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RestaurantController.class)
 public class RestaurantControllerTests {
@@ -71,6 +73,16 @@ public class RestaurantControllerTests {
                 .andExpect(content().string(
                         containsString("\"name\":\"Cyber Food\"")
                 ));
+    }
+    @Test
+    public void create() throws Exception {
+        Restaurant restaurant =new Restaurant(1234L,"BeRyong","Seoul");
+
+        mvc.perform(post("/restaurants"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location","/restaurants/1234"))
+                .andExpect(content().string("{}"));
+        verify(restaurantService).addRestaurant(any());
     }
 
 
